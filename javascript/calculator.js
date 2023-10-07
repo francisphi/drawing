a13f314e508bc9373c307533a3cc8ee202bbbcc0
Jess_francis_Aisen <128569963+francisphi@users.noreply.github.com>
2023-10-07 08:13:34 -0700

    Add files via upload

--- javascript/calculator.js
+++ javascript/calculator.js
@@ -3,31 +3,32 @@ let buttons = document.querySelectorAll('button');
 
 let string = '';
 
-buttons.forEach(element =>{
-    element.addEventListener('click', (b)=>{
-        if(b.target.innerText == '='){
-            string = String(eval(string))
+buttons.forEach(element => {
+    element.addEventListener('click', (b) => {
+        if (b.target.innerText == '=') {
+            // Replace 'x' with '*' for evaluation
+            string = String(eval(string.replace(/x/g, '*')));
             inputBox.value = string;
         }
-        //to delete all inputed
-        else if (b.target.innerText == 'AC'){
+        // To clear all input
+        else if (b.target.innerText == 'AC') {
             string = '';
             inputBox.value = string;
         }
-        //to delete one by one inputed
-        else if(b.target.innerText == 'DEL'){
-            string = string.substring(0,string.length-1)
+        // To delete one character at a time
+        else if (b.target.innerText == 'DEL') {
+            string = string.substring(0, string.length - 1);
             inputBox.value = string;
         }
-        else if(b.target.innerText == 'x²'){
+        else if (b.target.innerText == 'x²') {
             // Calculate the square of the current input
             string = String(eval(string + '*' + string));
             inputBox.value = string;
         }
-        else{
-            string += b.target.innerText;
+        else {
+            // Use 'x' for multiplication when appending to the string
+            string += (b.target.innerText === 'x') ? 'x' : b.target.innerText;
             inputBox.value = string;
         }
     })
 })
-
